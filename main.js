8939102d5b0e18c4fd989295e43258a6e42f49c4
Dwipta-Coder <85009949+Dwipta-Coder@users.noreply.github.com>
2021-08-06 18:55:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+block_image_width = 30;
+block_image_height = 30;
+
+player_x = 10;
+player_y = 10;
+
+var player_object = "";
+
+function player_update() {
+    fabric.Image.fromUrl("player.png", function (Img) {
+        player_object = Img;
+
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top: player_y,
+            left: player_x
+        });
+        canvas.add(player_object);
+
+    });
+}
+
+function new_image(get_image) {
+    fabric.Image.fromUrl(get_image, function (Img) {
+        block_image_object = Img;
+
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top: player_y,
+            left: player_x
+        });
+        canvas.add(block_image_object);
+
+    });
+
+}
